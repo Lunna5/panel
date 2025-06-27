0700095ffe94a63a5004406bacaf7b4580792ecc
Lunna5 <git@lunna.dev>
2025-06-27 19:29:33 +0200

    feat(application): enable transaction management and JPA repositories in PanelApplication

--- src/main/java/dev/lunna/panel/PanelApplication.java
+++ src/main/java/dev/lunna/panel/PanelApplication.java
@@ -3,11 +3,15 @@ package dev.lunna.panel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAsync
 @SpringBootApplication
+@EnableTransactionManagement
 @ConfigurationPropertiesScan
+@EnableJpaRepositories(basePackages = "dev.lunna.panel")
 public class PanelApplication {
   public static void main(String[] args) {
     SpringApplication.run(PanelApplication.class, args);
