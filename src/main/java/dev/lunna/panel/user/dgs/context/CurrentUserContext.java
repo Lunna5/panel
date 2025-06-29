8371507b60c139cd28bebed9761b038a7fe512d2
Lunna5 <git@lunna.dev>
2025-06-29 20:14:49 +0200

    refactor(user): simplify CurrentUserContext to a record for better readability

--- src/main/java/dev/lunna/panel/user/dgs/context/CurrentUserContext.java
+++ src/main/java/dev/lunna/panel/user/dgs/context/CurrentUserContext.java
@@ -1,17 +1,6 @@
 package dev.lunna.panel.user.dgs.context;
 
-import com.netflix.graphql.dgs.context.DgsContext;
-import com.netflix.graphql.dgs.context.DgsCustomContextBuilder;
 import dev.lunna.panel.user.ApplicationUser;
 
-public class CurrentUserContext {
-  private final ApplicationUser user;
-
-  public CurrentUserContext(ApplicationUser user) {
-    this.user = user;
-  }
-
-  public ApplicationUser getUser() {
-    return user;
-  }
+public record CurrentUserContext(ApplicationUser user) {
 }
