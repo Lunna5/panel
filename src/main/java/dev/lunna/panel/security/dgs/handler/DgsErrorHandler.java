c7ed0517d48c116d8cb8edcf27594bbc14aeb116
Lunna5 <git@lunna.dev>
2025-06-29 20:15:32 +0200

    feat(graphql): add DgsErrorHandler for handling JsonBaseException in GraphQL

---
+++ src/main/java/dev/lunna/panel/security/dgs/handler/DgsErrorHandler.java
@@ -0,0 +1,18 @@
+package dev.lunna.panel.security.dgs.handler;
+
+import com.netflix.graphql.types.errors.ErrorType;
+import dev.lunna.panel.core.exception.JsonBaseException;
+import graphql.GraphQLError;
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class DgsErrorHandler {
+  @GraphQlExceptionHandler
+  public GraphQLError handle(JsonBaseException ex) {
+    return GraphQLError.newError().errorType(ErrorType.BAD_REQUEST)
+        .message(ex.getError().tl())
+        .extensions(ex.getError().toMap())
+        .build();
+  }
+}
